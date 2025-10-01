28a0a0bf18221382a10f3fa1941fb9115df4f081
Cheng Wilson <98730459+wilson1070033@users.noreply.github.com>
2025-10-01 21:41:24 +0800

    Update tcp_socket.h
    Change to windows version

--- tcp_socket.h
+++ tcp_socket.h
@@ -2,34 +2,49 @@
 #ifndef TCP_SOCKET_H
 #define TCP_SOCKET_H
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <pthread.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <string>
+#include <memory>
 
-// 建立 TCP Socket
-int create_tcp_socket();
+#pragma comment(lib, "ws2_32.lib")
 
-// 設定 Socket 選項（解決 "Address already in use" 問題）
-int set_socket_reuse(int sock_fd);
+using namespace std;
 
-// 建立伺服器 Socket 並綁定
-int create_server_socket(const char* host, int port);
+// TCP Socket 類別，封裝 Windows Socket API
+class TCPSocket {
+private:
+    SOCKET sock_fd;
+    bool is_server;
+    bool wsa_initialized;
+    bool initialize_wsa();
+    void cleanup_wsa();
+public:
+    // 客戶端建構函式
+    TCPSocket();    
+    // 伺服器建構函式
+    TCPSocket(int port, const string& host = "0.0.0.0");    
+    // 解構函式
+    ~TCPSocket();    
+    // 連接到伺服器（客戶端）
+    bool connect(const string& host, int port);
+    // 開始監聽（伺服器）
+    bool listen(int backlog = 5);    
+    // 接受客戶端連線（伺服器）
+    unique_ptr<TCPSocket> accept();    
+    // 傳送資料
+    int send(const string& data);
+    // 接收資料
+    string recv(int buffer_size = 1024);    
+    // 關閉連線
+    void close();    
+    // 檢查 Socket 是否有效
+    bool is_valid() const;
+    // 取得最後的錯誤訊息
+    static string get_last_error();
+private:
+    // 用於 accept() 建立已連接的 Socket
+    TCPSocket(SOCKET connected_socket);
+};
 
-// 連接到伺服器
-int connect_to_server(const char* host, int port);
-
-// 傳送資料
-int send_data(int sock_fd, const char* data);
-
-// 接收資料
-int recv_data(int sock_fd, char* buffer, int buffer_size);
-
-// 關閉 Socket
-void close_socket(int sock_fd);
-
-#endif
+#endif // TCP_SOCKET_H
